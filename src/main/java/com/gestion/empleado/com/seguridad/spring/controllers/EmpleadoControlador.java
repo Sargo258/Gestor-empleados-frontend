155df4a90488ecfb11330dffbfebf26b4ffad5a2
santiago Rivas Gomez <santiagorg218@gmail.com>
2024-05-05 16:34:31 -0500

    se agrago el cross origin en el controlador

--- src/main/java/com/gestion/empleado/com/seguridad/spring/controllers/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleado/com/seguridad/spring/controllers/EmpleadoControlador.java
@@ -3,6 +3,7 @@ package com.gestion.empleado.com.seguridad.spring.controllers;
 import com.gestion.empleado.com.seguridad.spring.models.Empleado;
 import com.gestion.empleado.com.seguridad.spring.repository.EmpleadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController  
 @RequestMapping("api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmpleadoControlador {
 
     @Autowired
